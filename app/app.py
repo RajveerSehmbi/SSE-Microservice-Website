4b1d577c0670ffb14ac93bb07e9ca1bd5f999ab1
Radifire <Jaghoghov@gmail.com>
2024-01-29 18:50:44 +0000

    fixed http request and modified filtering mechanic

--- app/app.py
+++ app/app.py
@@ -4,19 +4,21 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/books', methods=['GET'])
 def books():
     queries = request.args.to_dict()
-    books = requests.get('https://sse-book-api-server.fpf7gvfpdfacbxby.uksouth.azurecontainer.io/books').json()
+    books = requests.get('http://sse-book-api-server.fpf7gvfpdfacbxby.uksouth.azurecontainer.io/books').json()
     filtered_books = []
+
     if queries:
         for book in books:
-            if all(query in book for query in queries):
+            # Check if each key-value pair in queries matches the corresponding pair in the book
+            if all(str(book.get(key, None)) == value for key, value in queries.items()):
                 filtered_books.append(book)
         return jsonify(filtered_books)
     else:
-        return books
-
+        return jsonify(books)  # Return all books if there are no query parameters
 
 if __name__ == '__main__':
     app.run(debug=True)
